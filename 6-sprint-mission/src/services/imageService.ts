4133192c3069b08006611dfcea3ce8bbbc82034a
Seung <ksh30916@naver.com>
2025-05-14 17:23:50 +0900

    refactor: 스프린트 미션 1~7폴더 제거

--- 6-sprint-mission/src/services/imageService.ts
+++
@@ -1,6 +0,0 @@
-import path from 'path';
-import { STATIC_PATH } from '../lib/constants';
-
-export function generateImageUrl(host: string, filename: string): string {
-  return `http://${path.join(host, STATIC_PATH, filename)}`;
-}
