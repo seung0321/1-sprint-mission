e6eef3d6936d4c9d35f90cd4b2e0f9a46fca279c
Seung <ksh30916@naver.com>
2025-05-15 10:33:32 +0900

    feat: 초기 타입 및 struct파일 작성

---
+++ typings/notification.ts
@@ -0,0 +1,22 @@
+import { NotificationType } from '@prisma/client';
+
+export type PriceFluctuationPayload = {
+  productId: number;
+  oldPrice: number;
+  newPrice: number;
+};
+
+export type CreateCommentPayload = {
+  articleId: number;
+  commentId: number;
+  content: string;
+};
+
+export type NotificationPayload = PriceFluctuationPayload | CreateCommentPayload;
+
+export interface NotificationResponse {
+  userId: number;
+  type: NotificationType;
+  payload: NotificationPayload;
+  read: boolean;
+}
