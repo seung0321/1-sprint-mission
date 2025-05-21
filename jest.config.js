da6b249c5e79a3a12d7c8851e297b2a0ac5a8c23
Seung <ksh30916@naver.com>
2025-05-21 18:43:05 +0900

    feat: test코드를 작성하기 위한 파일 재구성

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   testEnvironment: 'node',
   verbose: true,
-  testMatch: ['src/**/*.test.ts'],
+  testMatch: ['<rootDir>/src/**/*.test.ts'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
